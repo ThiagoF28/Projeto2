670b9d0ef52bcc30bd272bef05e049d15effb133
ronaldoroger22 <ronaldoroger12@gmail.com>
2023-11-21 20:11:39 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -1,51 +1,61 @@
 function calcularPlanos() {
-    var idade = parseInt(document.getElementById('idade').value);
-    var peso = parseFloat(document.getElementById('peso').value);
-    var altura = parseFloat(document.getElementById('altura').value);
-
-    var unimed = calUnimed(idade, peso, altura);
-    var hapvida = calHapvida(idade, peso, altura);
-
-    var resultadoHTML = "<h2>Resultados:</h2>";
-    resultadoHTML += "<table><tr><th></th><th>Básico</th><th>Standard</th><th>Premium</th></tr>";
-    resultadoHTML += "<tr><td>unimed</td><td>" + unimed.basico.toFixed(5) + "</td><td>" + unimed.standard.toFixed(5) + "</td><td>" + unimed.premium.toFixed(5) + "</td></tr>";
-    resultadoHTML += "<tr><td>hapvida</td><td>" + hapvida.basico.toFixed(5) + "</td><td>" + hapvida.standard.toFixed(5) + "</td><td>" + hapvida.premium.toFixed(5) + "</td></tr>";
-    resultadoHTML += "</table>";
-
-    document.getElementById('resultado').innerHTML = resultadoHTML;
-}
-
-function calUnimed(idade, peso, altura) {
-    var imc = peso / (altura * altura);
-    return {
-        basico: 100 + (idade * 10 * (imc / 10)),
-        standard: (150 + (idade * 15)) * (imc / 10),
-        premium: (200 - (imc * 10) + (idade * 20)) * (imc / 10),
+    const idade = parseInt(document.getElementById('idade').value);
+    const peso = parseFloat(document.getElementById('peso').value);
+    const altura = parseFloat(document.getElementById('altura').value);
+    
+    
+    const imc = peso / ((altura / 100) ** 2);
+    
+    const unimed = {
+    basic: 100 + (idade * 10 * (imc / 10)),
+    standard: (150 + (idade * 15)) * (imc / 10),
+    premium: (200 - (imc * 10) + (idade * 20)) * (imc / 10)
     };
-}
-
-function calHapvida(idade, peso, altura) {
-    var imc = peso / (altura * altura);
-    var calculComorbidade = calcularComorbidade(imc);
-    return {
-        basico: 100 + (calculComorbidade * 10 * (imc / 10)),
-        standard: (150 + (calculComorbidade * 15)) * (imc / 10),
-        premium: (200 - (imc * 10) + (calculComorbidade * 20)) * (imc / 10),
+    
+    let fatorComorbidade = 1;
+    
+    if (imc < 18.5) fatorComorbidade = 10;
+    else if (imc >= 18.5 && imc < 24.9) fatorComorbidade = 1;
+    else if (imc >= 25 && imc < 29.9) fatorComorbidade = 6;
+    else if (imc >= 30 && imc < 34.9) fatorComorbidade = 10;
+    else if (imc >= 35 && imc < 39.9) fatorComorbidade = 20;
+    else if (imc >= 40) fatorComorbidade = 30;
+    
+    
+    const hapvida = {
+    basic: 100 + (fatorComorbidade * 10 * (imc / 10)),
+    standard: (150 + (fatorComorbidade * 15)) * (imc / 10),
+    premium: (200 - (imc * 10) + (fatorComorbidade * 20)) * (imc / 10)
     };
-}
-
-function calcularComorbidade(imc) {
-    if (imc < 18.5) {
-        return 10;
-    } else if (imc < 24.9) {
-        return 1;
-    } else if (imc < 29.9) {
-        return 6;
-    } else if (imc < 34.9) {
-        return 10;
-    } else if (imc < 39.9) {
-        return 20;
-    } else if (imc >= 40.0) {
-        return 30;
+    if (altura > 0 && peso > 0 && idade > 0) {
+    
+    const resultTable = document.getElementById('tabelaResultado');
+    resultTable.innerHTML = `
+    <table>
+    <tr>
+    <th>Plano</th>
+    <th>Unimed</th>
+    <th>Hapvida</th>
+    </tr>
+    <tr>
+    <td>Básico</td>
+    <td>R$ ${unimed.basic.toFixed(2)}</td>
+    <td>R$ ${hapvida.basic.toFixed(2)}</td>
+    </tr>
+    <tr>
+    <td>Standard</td>
+    <td>R$ ${unimed.standard.toFixed(2)}</td>
+    <td>R$ ${hapvida.standard.toFixed(2)}</td>
+    </tr>
+    <tr>
+    <td>Premium</td>
+    <td>R$ ${unimed.premium.toFixed(2)}</td>
+    <td>R$ ${hapvida.premium.toFixed(2)}</td>
+    </tr>
+    </table>
+    `;
+    } else {
+    alert('Insira valores válidos.');
     }
+    
 }
